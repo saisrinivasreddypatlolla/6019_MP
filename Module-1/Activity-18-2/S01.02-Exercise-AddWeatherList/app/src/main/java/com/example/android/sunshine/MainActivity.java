84f8562c0d7c9d84a20276b122cda0efc149f520
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2019-03-12 14:25:18 +0530

    Activity-18

---
+++ Module-1/Activity-18-2/S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.sunshine;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.example.android.sunshine.utilities.OpenWeatherJsonUtils;
+
+public class MainActivity extends AppCompatActivity {
+
+    // TODO (1) Create a field to store the weather display TextView
+    TextView weatherDisplay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forecast);
+        weatherDisplay = (TextView) findViewById(R.id.tv_weather_data);
+
+        // TODO (2) Use findViewById to get a reference to the weather display TextView
+
+        // TODO (3) Create an array of Strings that contain fake weather data
+        String[] weatherData = {"Cloudy","Sunny","Rainy","Cold","Cloudy","Sunny","Rainy","Cold","Cloudy","Sunny","Rainy","Cold"};
+        // TODO (4) Append each String from the fake weather data array to the TextView
+        for(String weather:weatherData){
+            weatherDisplay.append(weather+"\n\n\n");
+        }
+    }
+}
