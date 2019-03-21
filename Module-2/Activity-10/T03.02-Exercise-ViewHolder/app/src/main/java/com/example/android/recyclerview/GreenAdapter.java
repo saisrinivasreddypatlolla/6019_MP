2fe775237e0f7b340553eeb2d37ff203a9813164
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2019-03-22 02:41:04 +0530

    Activity-10

--- Module-2/Activity-10/T03.02-Exercise-ViewHolder/app/src/main/java/com/example/android/recyclerview/GreenAdapter.java
+++ Module-2/Activity-10/T03.02-Exercise-ViewHolder/app/src/main/java/com/example/android/recyclerview/GreenAdapter.java
@@ -117,7 +117,7 @@ public class GreenAdapter extends RecyclerView.Adapter<GreenAdapter.NumberViewHo
         return mNumberItems;
     }
 
-    // TODO (12) Create a class called NumberViewHolder that extends RecyclerView.ViewHolder
+    // TODo (12) Create a class called NumberViewHolder that extends RecyclerView.ViewHolder
         class NumberViewHolder extends RecyclerView.ViewHolder {
             TextView listItemNumberView;
 
