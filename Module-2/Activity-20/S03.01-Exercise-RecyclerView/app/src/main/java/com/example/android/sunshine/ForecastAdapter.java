907ad54e7321da6ff5f1c50b272722be0c798b6f
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2019-03-21 23:36:09 +0530

    Activity-20

---
+++ Module-2/Activity-20/S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
@@ -0,0 +1,80 @@
+package com.example.android.sunshine;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.ForecastAdapterViewHolder> {
+    // TODO (15) Add a class file called ForecastAdapter
+    // TODO (22) Extend RecyclerView.Adapter<ForecastAdapter.ForecastAdapterViewHolder>
+
+    // TODO (23) Create a private string array called mWeatherData
+    String[] mWeatherData;
+    // TODO (47) Create the default constructor (we will pass in parameters in a later lesson)
+    ForecastAdapter(){
+
+    }
+    // TODO (16) Create a class within ForecastAdapter called ForecastAdapterViewHolder
+    public  class ForecastAdapterViewHolder extends RecyclerView.ViewHolder{
+        public final TextView mWeatherTextView;
+        ForecastAdapterViewHolder(View view){
+            super(view);
+            mWeatherTextView = (TextView) view.findViewById(R.id.tv_weather_data);
+        }
+    }
+    // TODO (17) Extend RecyclerView.ViewHolder
+
+    // Within ForecastAdapterViewHolder ///////////////////////////////////////////////////////////
+    // TODO (18) Create a public final TextView variable called mWeatherTextView
+
+    // TODO (19) Create a constructor for this class that accepts a View as a parameter
+    // TODO (20) Call super(view) within the constructor for ForecastAdapterViewHolder
+    // TODO (21) Using view.findViewById, get a reference to this layout's TextView and save it to mWeatherTextView
+    // Within ForecastAdapterViewHolder ///////////////////////////////////////////////////////////
+
+    @NonNull
+    @Override
+    public ForecastAdapterViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        Context context = viewGroup.getContext();
+        int layoutIdForListItem = R.layout.forecast_list_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+
+        View view = inflater.inflate(layoutIdForListItem, viewGroup, shouldAttachToParentImmediately);
+        return new ForecastAdapterViewHolder(view);
+    }
+
+
+    // TODO (24) Override onCreateViewHolder
+    // TODO (25) Within onCreateViewHolder, inflate the list item xml into a view
+    // TODO (26) Within onCreateViewHolder, return a new ForecastAdapterViewHolder with the above view passed in as a parameter
+
+    @Override
+    public void onBindViewHolder(@NonNull ForecastAdapterViewHolder forecastAdapterViewHolder, int i) {
+        String weatherForThisDay = mWeatherData[i];
+        forecastAdapterViewHolder.mWeatherTextView.setText(weatherForThisDay);
+    }
+
+    // TODO (27) Override onBindViewHolder
+    // TODO (28) Set the text of the TextView to the weather for this list item's position
+
+    // TODO (29) Override getItemCount
+    // TODO (30) Return 0 if mWeatherData is null, or the size of mWeatherData if it is not null
+
+    @Override
+    public int getItemCount() {
+        if (null == mWeatherData) return 0;
+        return mWeatherData.length;
+    }
+
+    // TODO (31) Create a setWeatherData method that saves the weatherData to mWeatherData
+    // TODO (32) After you save mWeatherData, call notifyDataSetChanged
+    public void setWeatherData(String[] weatherData) {
+        mWeatherData = weatherData;
+        notifyDataSetChanged();
+    }
+}
